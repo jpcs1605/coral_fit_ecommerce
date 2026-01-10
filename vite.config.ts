ead070177ccc4d200c0cef93957b6b633c2b0ab7
Joao Souza <joao.souza@arizona.global>
2026-01-10 10:57:24 -0300

    config de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/E-commerce Fashion Website/',
+  base: '/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
